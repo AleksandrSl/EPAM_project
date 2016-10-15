73205c9923f17abc378c6bc2d9f758ed9d9394ff
aleksandrsl <Alex121994@mail.ru>
2016-10-16 01:48:02 +0300

    Change directory for MetaWF

---
+++ bin/first_pipeline/MetaWF.py
@@ -0,0 +1,27 @@
+import sciluigi as sl
+import luigi
+import glob
+from Workflow import TestWF
+import Components
+
+class MetaWF(sl.WorkflowTask):
+    '''
+    Meta workflow
+    '''
+    #path_to_fastq = '/home/aleksandrsl/Desktop/EPAM_project/data/sequences/'   # Real
+    path_to_fastq = '/home/aleksandrsl/Desktop/EPAM_project/data/first_try/sequences/'  # Test
+
+    def workflow(self):
+
+        tasks = []
+        iterator = iter(sorted(glob.glob(self.path_to_fastq + '*.fastq.gz')))
+        for fastq in iterator:
+            paired_fastq = (fastq, next(iterator))
+            print(paired_fastq)
+            wf = self.new_task('TestWf', TestWF, paired_fastq=paired_fastq)
+            tasks.append(wf)
+        return tasks
+
+if __name__ == '__main__':
+    #luigi.run(local_scheduler=True, main_task_cls=MetaWF)
+    sl.run_local(main_task_cls=MetaWF)
