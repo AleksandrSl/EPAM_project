92c9e2885da4f8b144bd5f1dcab21e42595c54ed
aleksandrsl <Alex121994@mail.ru>
2016-10-16 01:43:59 +0300

    Create fastqc workflow

--- bin/first_pipeline/Components.py
+++ bin/first_pipeline/Components.py
@@ -1,20 +1,50 @@
 import luigi
+import logging
 import sciluigi as sl
+import glob
+from subprocess import call
 
 class FastqcTask(sl.Task):
 
     # Parameter
-    text = luigi.Parameter()
+    fastq_file_path = None
 
     # I/O
-
     def fastqc_res(self):
-        return sl.TargetInfo(self, 'data/' + self.text)
+        pass
+        # return sl.TargetInfo(self, 'data/' + self.text)
 
     # Implementation
     def run(self):
-        return sl.TargetInfo(self )
+        #call('mkdir /home/aleksandrsl/Desktop/EPAM_project/data/first_try/fastq_res_v2', shell=True)
+        cmd = '~/Desktop/EPAM_project/tools/fastqc/fastqc -o /home/aleksandrsl/Desktop/EPAM_project/data/first_try/fastq_res_v2 ' + self.fastq_file_path
+        print('!!!!' + cmd)
+        # fastqc [-o output dir] [--(no)extract] [-f fastq|bam|sam]  [-c contaminant file] seqfile1 .. seqfileN
+        # logging.log.info("COMMAND TO EXECUTE: " + cmd)
+        call(cmd, shell=True)
 
-    cmd = 'cat ' + self.in_data().path + ' | sed "s/A/T/g" > ' + self.out_replatot().path
-    log.info("COMMAND TO EXECUTE: " + cmd)
-    call(cmd, shell=True)
+class FastqcTask_v2(sl.Task):
+
+    # Parameter
+    fastq_file_path = None
+
+    # I/O
+    def fastqc_res(self):
+        pass
+        # return sl.TargetInfo(self, 'data/' + self.text)
+
+    # Implementation
+    def run(self):
+        #call('mkdir /home/aleksandrsl/Desktop/EPAM_project/data/first_try/fastq_res_v2', shell=True)
+        cmd = '~/Desktop/EPAM_project/tools/fastqc/fastqc -o /home/aleksandrsl/Desktop/EPAM_project/data/first_try/fastq_res_v2 ' + self.fastq_file_path[0] + ' ' + self.fastq_file_path[1]
+        print('!!!!' + cmd)
+        # fastqc [-o output dir] [--(no)extract] [-f fastq|bam|sam]  [-c contaminant file] seqfile1 .. seqfileN
+        # logging.log.info("COMMAND TO EXECUTE: " + cmd)
+        call(cmd, shell=True)
+
+
+# class RawSequencesTask(sl.ExternalTask):
+#
+#
+#     def out_raw_sequences(self):
+#         return sl.TargetInfo(self, 'BK0020_S1_L001_R1_001.fastq.gz')
--- bin/first_pipeline/Workflow.py
+++ bin/first_pipeline/Workflow.py
@@ -1,7 +1,44 @@
+import logging
 import luigi
 import sciluigi as sl
+from Components import RawSequencesTask, FastqcTask, FastqcTask_v2
 
+# ========================================================================
 
+log = logging.getLogger('sciluigi-interface')
 
+# ========================================================================
 
+class TestWF(sl.WorkflowTask):
+
+    # task = luigi.Parameter()
+    paired_fastq = luigi.TupleParameter()
+    #fastq_1, fastq_2 = paired_fastq
+
+    def workflow(self):
+        # First variant
+
+        # fastqc_1 = self.new_task('fastqc_1', FastqcTask)
+        # fastqc_2 = self.new_task('fastqc_2', FastqcTask)
+        #
+        # self.fastq_1, self.fastq_2 = self.paired_fastq
+        #
+        # Workflow definition
+
+        # fastqc_1.fastq_file_path = self.fastq_1
+        # fastqc_2.fastq_file_path = self.fastq_2
+
+        # Second variant
+        fastqc = self.new_task('fastqc', FastqcTask_v2)
+        fastqc.fastq_file_path = self.paired_fastq
+        #
+
+        # for name, instance in locals().iteritems():
+        #     if issubclass(type(instance), sl.Task):
+        #         log.info('{n}, task id: {taskid}\n{n}, hash: {h}',
+        #                 n=name,
+        #                 taskid=instance.task_id,
+        #                 h=instance.__hash__())
+
+        return locals()
 
